c0a429726a765231d89de89d40e02653809b51bb
remkha.bohdan.cr <remkha.bohdan.cr@gmail.com>
2023-07-10 15:11:30 +0300

    Save current route in localStorage

--- src/components/RouteCard.jsx
+++ src/components/RouteCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useUpdateRouteData } from "../context/routeContext";
+import storageKeys from "../constants/storageKeys";
 
 const RouteCard = ({
     route
@@ -10,7 +11,10 @@ const RouteCard = ({
     return (
         <button 
             className='route-card border-ghost px-6 py-8 bg-opacity-20 bg-slate-600 hover:glitch'
-            onClick={() => updateRoute(key)}
+            onClick={() => {
+                localStorage.setItem(storageKeys.currentRoute, key);
+                updateRoute(key);
+            }}
         >
             <div className="w-32 h-32 mb-5">
                 {logo && <img className="w-full h-full rounded-lg border-none" src={logo} alt="" />}
