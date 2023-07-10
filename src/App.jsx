af66491c9fa6551741b792ad1ae254fa94bf4487
remkha.bohdan.cr <remkha.bohdan.cr@gmail.com>
2023-07-10 17:25:20 +0300

    Added drag'n'drop

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import routes, { baseRoute } from './constants/routes';
 import storageKeys from './constants/storageKeys';
 import { useRouteData, useUpdateRouteData } from './context/routeContext';
 
+const navLabel = 'Back';
+
 const footerMessage = 'Created by CoralHeavens <3';
 const footerLink = 'https://github.com/CoralHeavens';
 
 function App() {
   const routeKey = useRouteData();
+  const route = routes[routeKey];
   const updateRoute = useUpdateRouteData();
   
   useEffect(() => {
@@ -18,8 +21,25 @@ function App() {
   }, [updateRoute, routeKey])
   
   return (
-    <div className='relative w-full h-[100vh] bg-slate-900 flex justify-center p-10 pt-20'>
-      {routes[routeKey]?.component}
+    <div className='relative w-full h-[100vh] bg-slate-900 flex flex-col items-center p-10'>
+      <nav className='mb-6 w-full flex items-center'>
+        {routeKey !== baseRoute.key && (
+          <>
+            <button className='absolute border-ghost px-3 py-2' onClick={() => {
+              updateRoute(baseRoute.key);
+              localStorage.setItem(storageKeys.currentRoute, baseRoute.key)
+            }}>
+              <div className='glitch' title={navLabel}>
+                {navLabel}
+              </div>
+            </button>
+            <h2 className='mx-auto text-5xl leading-7'>
+              {route?.label}
+            </h2>
+          </>
+        )}
+      </nav>
+      {route?.component}
       <footer className='absolute bottom-2 opacity-30'>
         <a className='text-base' href={footerLink}>
           {footerMessage}
--- src/components/Spider.jsx
+++ src/components/Spider.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { ReactSortable } from "react-sortablejs";
 
 const Spider = () => {
+    const [state, setState] = useState([
+        { id: 1, name: "Card 1", suit: 'spades' },
+        { id: 2, name: "Card 2", suit: 'spades' },
+        { id: 3, name: "Card 3", suit: 'clubs' },
+    ]);
+    
     return (
-        <div>Spider</div>
+        <section className="field bg-slate-200">
+            <ReactSortable 
+                animation={200} 
+                list={state} 
+                setList={setState}
+                group='suit'
+                className="flex gap-4 p-4"
+            >
+                {state.map((item) => (
+                    <div className='cursor-pointer bg-slate-500' key={item.id}>{item.name}</div>
+                ))}
+            </ReactSortable>
+        </section>
     )
 }
 
