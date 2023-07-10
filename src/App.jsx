fbc66cb8f44ae568ec0441501c4dff5973ce3c83
remkha.bohdan.cr <remkha.bohdan.cr@gmail.com>
2023-07-10 10:37:17 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main className='w-full h-[100vh] bg-slate-900 flex justify-center lg:p-10'>
+      Solitaire
+    </main>
+  );
+}
+
+export default App;
